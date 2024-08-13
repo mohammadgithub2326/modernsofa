760497cf153da6d10663f55e0be9f633746eeaf9
mohammad noor <g.s.dadanoor@gmail.com>
2024-08-13 17:52:22 +0530

    commit2

--- src/app/(pages)/registration/page.jsx
+++ src/app/(pages)/registration/page.jsx
@@ -1,7 +1,7 @@
 "use client"
 import { useState } from 'react';
 import axios from 'axios';
-import styles from './Register.module.css';
+import styles from './register.module.css';
 
 const Register = () => {
     const [formData, setFormData] = useState({
