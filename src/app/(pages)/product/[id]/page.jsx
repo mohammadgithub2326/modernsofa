40f2fd931cc70b643ff983c44474da585cab8fc5
mohammad noor <g.s.dadanoor@gmail.com>
2024-09-14 21:39:32 +0530

    semi commit9

--- src/app/(pages)/product/[id]/page.jsx
+++ src/app/(pages)/product/[id]/page.jsx
@@ -105,15 +105,15 @@ const ProductDetails = (props) => {
       const productId = id
       // API call to order 
       startLoading();
-      // axios.post('http://localhost:5000/api/v1/orders/create', {
-      axios.post('https://modernsofabk.onrender.com/api/v1/orders/create', {
+      axios.post('http://localhost:5000/api/v1/orders/create', {
+      // axios.post('https://modernsofabk.onrender.com/api/v1/orders/create', {
             userId,
             productId
           }, {
             headers: { Authorization: accessToken }
           }) 
-          startLoading()
           .then(() => {
+            stopLoading();
            alert('Product ordered!')})
           .catch(error => console.log(error));
       console.log(`Product ${id} ${product?.name} ordered  for user ${userId}`);
